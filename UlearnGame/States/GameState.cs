5c367b51059b4a0b57720f1368da1d01e5f79351
monrotears <dmitriy2000151@gmail.com>
2026-05-11 14:02:57 +0500

    Add second player mode

--- UlearnGame/States/GameState.cs
+++ UlearnGame/States/GameState.cs
@@ -38,29 +38,39 @@ namespace UlearnGame.States
                     Position = new Vector2(Game1.ScreenWidth / 2, Game1.ScreenHeight / 2),
                 },
 
-                new Player(playerTexture)
-                {
-                    Colour = Color.White,
-                    Position = new Vector2(100, 250),
-                    Layer = 0.3f,
-                    Input = new Models.Input()
-                    {
-                        Up = Keys.W,
-                        Down = Keys.S,
-                        Left = Keys.A,
-                        Right = Keys.D,
-                        Shoot = Keys.None,
-                    },
-                    Health = 20,
-                    Score = new Models.Score()
-                    {
-                        PlayerName = "Игрок 1",
-                        Value = 0,
-                    },
-                }
+                CreatePlayer(playerTexture, Color.White, new Vector2(100, 250), "Игрок 1", Keys.W, Keys.S, Keys.A, Keys.D)
             };
 
+            if (PlayerCount >= 2)
+            {
+                _sprites.Add(CreatePlayer(playerTexture, Color.LightBlue, new Vector2(100, 350), "Игрок 2", Keys.Up, Keys.Down, Keys.Left, Keys.Right));
+            }
+
             _players = _sprites.Where(sprite => sprite is Player).Select(sprite => (Player)sprite).ToList();
+        }
+
+        private Player CreatePlayer(Texture2D playerTexture, Color colour, Vector2 position, string playerName, Keys up, Keys down, Keys left, Keys right)
+        {
+            return new Player(playerTexture)
+            {
+                Colour = colour,
+                Position = position,
+                Layer = 0.3f,
+                Input = new Models.Input()
+                {
+                    Up = up,
+                    Down = down,
+                    Left = left,
+                    Right = right,
+                    Shoot = Keys.None,
+                },
+                Health = 20,
+                Score = new Models.Score()
+                {
+                    PlayerName = playerName,
+                    Value = 0,
+                },
+            };
         }
 
         public override void Update(GameTime gameTime)
@@ -88,7 +98,11 @@ namespace UlearnGame.States
 
             spriteBatch.Begin();
             spriteBatch.DrawString(_font, "Prototype build: movement only", new Vector2(40, 10), Color.Red);
-            spriteBatch.DrawString(_font, "WASD - движение, Esc - выход", new Vector2(40, 35), Color.Red);
+            spriteBatch.DrawString(_font, "WASD - движение игрока 1, Esc - выход", new Vector2(40, 35), Color.Red);
+
+            if (PlayerCount >= 2)
+                spriteBatch.DrawString(_font, "Стрелки - движение игрока 2", new Vector2(40, 60), Color.Red);
+
             spriteBatch.End();
         }
     }
